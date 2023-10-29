8acdf7b37df2d14698707a8a538c2ad6fbddd887
radiantwf <radiantwf@gmail.com>
2023-10-30 03:21:36 +0800

    1

--- prepare.sh
+++ prepare.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-python3.12 -m venv venv
+python3.11 -m venv venv
 
 source ./venv/bin/activate
 
 pip install --upgrade pip --index-url http://mirrors.aliyun.com/pypi/simple/ --trusted-host mirrors.aliyun.com
 pip install -r requirements.txt  --index-url http://mirrors.aliyun.com/pypi/simple/ --trusted-host mirrors.aliyun.com
 
-auto-py-to-exe
+#auto-py-to-exe
--- yolo_training/train.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-yolo detect train data=train.yaml model=yolov8n.pt epochs=10 lr0=0.01
