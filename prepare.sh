5793b26b430fa82a9e5ff68aeee8a8f59e747d62
wangfeng <wangfeng@jyinform.com>
2023-11-29 15:06:19 +0800

    1

--- prepare.sh
+++ prepare.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-python3.11 -m venv venv
+python3.12 -m venv venv
 
 source ./venv/bin/activate
 
--- yolo_training/labelimg-install.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-pip install -i https://pypi.tuna.tsinghua.edu.cn/simple labelimg
