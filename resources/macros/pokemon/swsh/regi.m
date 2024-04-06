36c534b4aeda9abb7e3eab48b6c6a03b10e24618
radiantwf <radiantwf@gmail.com>
2024-04-06 16:39:49 +0800

    增加 剑盾 - 饲育屋复制道具 脚本

--- resources/macros/pokemon/swsh/regi.m
+++ resources/macros/pokemon/swsh/regi.m
@@ -1,5 +1,5 @@
 --雷吉艾勒奇--
-<regieleki|雷吉艾勒奇（定点）|999999--secondary|副设备(副设备启动游戏时校验)|False>
+<regieleki|剑盾 - 雷吉艾勒奇（定点）|999999--secondary|副设备(副设备启动游戏时校验)|False>
 {
 	[pokemon.swsh.common.restart_game]
 	body:
@@ -37,7 +37,7 @@
 }
 
 --雷吉铎拉戈--
-<regidrago|雷吉铎拉戈（定点）|999999--secondary|副设备(副设备启动游戏时校验)|True>
+<regidrago|剑盾 - 雷吉铎拉戈（定点）|999999--secondary|副设备(副设备启动游戏时校验)|True>
 {
 	[pokemon.swsh.common.restart_game]
 	body:
@@ -78,7 +78,7 @@
 }
 
 --雷吉艾斯--
-<regice|雷吉艾斯（定点）|999999--secondary|副设备(副设备启动游戏时校验)|True>
+<regice|剑盾 - 雷吉艾斯（定点）|999999--secondary|副设备(副设备启动游戏时校验)|True>
 {
 	[pokemon.swsh.common.restart_game]
 	body:
@@ -115,7 +115,7 @@
 }
 
 --雷吉洛克/雷吉斯奇鲁--
-<regirock|雷吉洛克/雷吉斯奇鲁（定点）|999999--secondary|副设备(副设备启动游戏时校验)|True>
+<regirock|剑盾 - 雷吉洛克/雷吉斯奇鲁（定点）|999999--secondary|副设备(副设备启动游戏时校验)|True>
 {
 	[pokemon.swsh.common.restart_game]
 	body:
