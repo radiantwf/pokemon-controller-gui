753088151b2b8eea53a5fff4976c53a3faae0dfe
radiantwf <radiantwf@gmail.com>
2024-05-30 11:09:12 +0800

    1

--- src/recognition/scripts/games/pokemon/swsh/dynamax_adventures/step02_choose_path.py
+++ src/recognition/scripts/games/pokemon/swsh/dynamax_adventures/step02_choose_path.py
@@ -2,6 +2,7 @@ import math
 from recognition.scripts.base.base_script import BaseScript
 from recognition.scripts.base.base_sub_step import BaseSubStep, SubStepRunningStatus
 import cv2
+import time
 
 
 class SWSHDAChoosePath(BaseSubStep):
@@ -16,6 +17,7 @@ class SWSHDAChoosePath(BaseSubStep):
             "resources/img/recognition/pokemon/swsh/dynamax_adventures/choose_path.png")
         self._choose_path_template = cv2.cvtColor(
             self._choose_path_template, cv2.COLOR_BGR2GRAY)
+        self._process_step_2_start_monotonic = 0
 
     def _process(self) -> SubStepRunningStatus:
         self._status = self.running_status
@@ -35,6 +37,8 @@ class SWSHDAChoosePath(BaseSubStep):
     def _process_steps(self):
         return [
             self.process_steps_0,
+            self.process_steps_1,
+            self.process_steps_2,
         ]
 
     def process_steps_0(self):
@@ -43,25 +47,46 @@ class SWSHDAChoosePath(BaseSubStep):
                 f"{'A' if self._leave_event else 'B'}:0.1->0.4", block=True, loop=28)
             self._process_step_index += 1
             self.time_sleep(0.5)
+            self._status = SubStepRunningStatus.OK
+            return
+        self._process_step_index += 1
+
+    def process_steps_1(self):
+        if self._process_step_2_start_monotonic == 0:
+            self._process_step_2_start_monotonic = time.monotonic()
+            self._check_times = 0
+        if time.monotonic() - self._process_step_2_start_monotonic > 30:
+            self._process_step_2_start_monotonic = 0
+            self._status = SubStepRunningStatus.OK
             return
         current_frame = self.script.current_frame
         gray_frame = cv2.cvtColor(current_frame, cv2.COLOR_BGR2GRAY)
         if not self._match_choose_path(gray_frame):
             if self._battle_index > 0:
-                self.script.macro_text_run(
-                    f"{'A' if self._enter_event else 'B'}:0.1", block=True)
-                self.time_sleep(0.4)
+                if self._check_times >= 2:
+                    self.script.macro_text_run(
+                        f"{'A' if self._enter_event else 'B'}:0.1", block=True)
+                    self._check_times = 0
+                    self.time_sleep(0.3)
+                else:
+                    self._check_times += 1
+                    self.time_sleep(0.2)
+            else:
+                self.time_sleep(0.5)
             return
         else:
-            if self._path < 0:
-                self.script.macro_text_run("LEFT:0.1->0.3", block=True,loop=math.fabs(self._path))
-            elif self._path > 0:
-                self.script.macro_text_run("RIGHT:0.1->0.3", block=True,loop=self._path)
-            self.script.macro_text_run(f"A:0.1->0.4", block=True)
-            self.script.macro_text_run(
-                f"{'A' if self._leave_event else 'B'}:0.1->0.4", block=True, loop=28)
             self._process_step_index += 1
-            self.time_sleep(0.5)
+
+    def process_steps_2(self):
+        if self._path < 0:
+            self.script.macro_text_run("LEFT:0.1->0.3", block=True,loop=math.fabs(self._path))
+        elif self._path > 0:
+            self.script.macro_text_run("RIGHT:0.1->0.3", block=True,loop=self._path)
+        self.script.macro_text_run(f"A:0.1->0.4", block=True)
+        self.script.macro_text_run(
+            f"{'A' if self._leave_event else 'B'}:0.1->0.4", block=True, loop=28)
+        self.time_sleep(0.5)
+        self._process_step_index += 1
 
     def _match_choose_path(self, gray) -> bool:
         crop_x, crop_y, crop_w, crop_h = 232, 432, 476, 72
